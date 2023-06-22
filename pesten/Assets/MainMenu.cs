4b74462e93f9701a0bd96c6d9868ed98129f005a
RohitNathie <103172111+RohitNathie@users.noreply.github.com>
2023-06-22 10:38:53 +0200

    Added main menu music

--- pesten/Assets/MainMenu.cs
+++ pesten/Assets/MainMenu.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip backgroundSound;
+
+    private void Start()
+    {
+        if (backgroundSound != null)
+        {
+            audioSource.clip = backgroundSound;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame ()
+    public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
-
     }
 }
