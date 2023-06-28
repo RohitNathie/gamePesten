50e89d65bb8b70296518f1668bb53d8634cc9a32
Rohan Schippers <rohan.schippers@gmail.com>
2023-06-28 21:55:19 +0200

    Uploaded the correct Deck.cs
    Uploaded the first version of Deck.cs on accident

--- pesten/Assets/Scenes/Deck.cs
+++ pesten/Assets/Scenes/Deck.cs
@@ -1,25 +1,39 @@
-using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-    public List<Card> cards; // List of all the cards in the deck
+    public List<Card> cards = new List<Card>();
+
+    private int currentIndex = 0;
 
     public void Shuffle()
     {
-        // Implement the logic to shuffle the deck
+        // Fisher-Yates shuffle algorithm
+        for (int i = 0; i < cards.Count - 1; i++)
+        {
+            int randomIndex = Random.Range(i, cards.Count);
+            Card temp = cards[randomIndex];
+            cards[randomIndex] = cards[i];
+            cards[i] = temp;
+        }
     }
 
     public Card DrawCard()
     {
-        // Implement the logic to draw a card from the deck
-        // This involves removing a card from the list and returning it
+        if (currentIndex < cards.Count)
+        {
+            Card drawnCard = cards[currentIndex];
+            currentIndex++;
+            return drawnCard;
+        }
+
         return null;
     }
 
     public void AddCard(Card card)
     {
-        // Implement the logic to add a card to the deck
-        // This involves adding the card to the list
+        cards.Add(card);
     }
 }
